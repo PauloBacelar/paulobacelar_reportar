9a7dd539362526affa3a309267febccde8457c4d
Paulo Andrade <paulobacelara@gmail.com>
2022-02-20 18:28:56 -0300

    Inserted Button using React Native Paper

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 import RadioInput from "./RadioInput";
 import TextUserInput from "./TextUserInput";
 
@@ -18,7 +19,18 @@ const Form = () => {
         <TextUserInput
           paddingBottom="7rem"
           placeholder="Especifique aqui o que houve."
+          style={{ marginBottom: "2rem" }}
         />
+
+        <Button
+          uppercase={false}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+          color="#07689f"
+          style={styles.button}
+        >
+          <Text style={styles.text}>Reportar</Text>
+        </Button>
       </View>
     </View>
   );
@@ -27,6 +39,16 @@ const Form = () => {
 const styles = StyleSheet.create({
   content: {
     marginHorizontal: "1.5rem",
+  },
+
+  button: {
+    borderRadius: 2,
+    height: "2.5rem",
+    justifyContent: "center",
+  },
+
+  text: {
+    fontFamily: "ubuntu",
   },
 });
 
